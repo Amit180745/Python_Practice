682f0739c7ed50b8f47aa422d4142c66dcb749c0
devlord9000 <your-devlord9000.com>
2026-01-08 23:12:58 +0530

    Break&Continue_statement

---
+++ pg5.py
@@ -0,0 +1,12 @@
+for i in range(1,21):
+    if i == 15:
+        break
+    print(i)
+
+print('Continue statement Starts from here!')
+
+for i in range(1,21):
+    if i == 15:
+        continue
+    print(i)
+    
